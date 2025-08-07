8588fe94ec45382f8f579d06d7693ae945bea576
Madelyn Arana <madelynarana@gmail.com>
2025-08-06 19:10:16 -0600

    Archivos iniciales.

---
+++ backend/src/test/java/com/parqueo/ParqueoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parqueo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
