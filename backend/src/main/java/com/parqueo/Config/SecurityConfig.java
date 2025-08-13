edb18bf1a626957f88f0b0aa2e62edfa381bc3df
Madelyn Arana <madelynarana@gmail.com>
2025-08-12 18:29:46 -0600

    Añadir Archivo de seguridad.

---
+++ backend/src/main/java/com/parqueo/Config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.parqueo.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        System.out.println("Configuracion SecurityFilterChain");
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("parqueo/*").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+}
+
